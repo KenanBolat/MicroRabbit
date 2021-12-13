d517ce405e97ad48ea8fa0ecf2ee17f03370324e
Kenan BOLAT <kenan.bolat@yandex.com>
2021-12-13 15:51:16 +0300

    Initial Commit

---
+++ MicrpRabbit/MicroRabbit.Ioc/DependencyContainor.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Ioc
+{
+    public class DependencyContainor 
+    {
+        public static void RegisterServices(IServiceCollection services)
+        
+        {
+            //DomainBus 
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+
+    }
+}
